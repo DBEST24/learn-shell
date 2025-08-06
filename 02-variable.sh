7533b45eacc78533d67e1c485160d4e0fb8b195a
Darshak24 <117837412+Darshak24@users.noreply.github.com>
2025-08-06 10:42:03 -0400

    Change on server

---
+++ 02-variable.sh
@@ -0,0 +1,12 @@
+# if we assign a name to a set of data oy os called as variable.
+
+# syntax: variable=data
+
+x=10   #x is variable
+
+# access the variable - $x or ${x}
+
+echo hello
+
+echo value of x is - $x
+
---
+++ 03-functions.sh
@@ -0,0 +1,12 @@
+# if we assign to the set of commands the it is a functions
+
+#syntax:
+# function function_name() {
+# commands
+#}
+
+# example
+
+SAMPLE(){
+  echo sample functio
+}
