d9646d3a6cbbb4c7285a4f1b29c8afa4314b22ee
Darshak24 <117837412+Darshak24@users.noreply.github.com>
2025-08-06 11:59:20 -0400

    Change on server

--- 02-variable.sh
+++ 02-variable.sh
@@ -16,10 +16,24 @@ echo value of x is - $x
 # on cli I send the variable using the following command
 
 # export a_env=1000
-
+# bash 02-variable.sh
 echo Env Variable - a_env - S{a_env}
 
 # LSH  - before executing the script
 # on cli
-
+# a_lsh=999 bash 02-variable.sh
 echo LSH varibale a_lsh - ${a_lsh}
+
+
+# RHS
+# S1 -Frist value to RHS
+# $2 - Second valur to RHS
+# $# - count number of values
+# $* - all values
+
+# on cli we will access the variable using the command
+
+# bash 02-variable.sh 1000 abc
+echo Frist value - $1
+echo second value - $2
+echo All values - $*
